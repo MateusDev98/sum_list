3f903133a456354ae8121b9cca7c2ce0ba011e53
Mateus (Dev) <mateus.lopes9812@gmail.com>
2023-08-15 09:51:53 -0300

    Finalização

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,18 +1,55 @@
 defmodule SumList do
-  @moduledoc """
-  Documentation for `SumList`.
-  """
-
   @doc """
-  Hello world.
+  Chama a função sum com a lista fornecida e o valor inicial do acumulador de 0.
 
-  ## Examples
+  ## Exemplos
 
-      iex> SumList.hello()
-      :world
+    iex> SumList.call([1, 2, 3])
+    6
+
+  Recebe uma lista e retorna a soma de todos os elementos da lista.
+
+  ## Parâmetros
+
+  - `list` - A lista de números a serem somados.
+
+  ## Retorno
+
+  Retorna a soma de todos os elementos da lista.
 
   """
-  def hello do
-    :world
+  def call(list), do: sum(list, 0)
+
+  def call_enum_sum(list), do: Enum.sum(list)
+
+  def call_enum_max(list), do: Enum.max(list)
+
+  def call_enum_min(list), do: Enum.min(list)
+
+  def call_enum_member(list), do: Enum.member?(list, 3)
+
+  def call_enum_any(list), do: Enum.any?(list, fn x -> x >= 3 end)
+
+  def call_enum_map(list), do: Enum.map(list, fn item -> item * 2 end)
+
+  def call_enum_map_tuple(tuple), do: Enum.map(tuple, fn item -> item end)
+
+  def call_enum_map_ignored(list) do
+    [_head | tail] = list
+    IO.inspect(_head, label: "head")
+    Enum.map(list, fn x -> x + 2 end)
+  end
+
+  def call_enum_map(list, multiplier) do
+    Enum.map(list, fn x -> multiply(x, multiplier) end)
+  end
+
+  defp multiply(a, b), do: a * b
+
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
   end
 end
--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,8 +1,35 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe("call/1") do
+    test "sum of empty list is 0" do
+      list = []
+
+      response = SumList.call(list)
+
+      exepected_response = 0
+
+      assert response == exepected_response
+    end
+
+    test "sum of [1, 2, 3] is 6" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      exepected_response = 6
+
+      assert response == exepected_response
+    end
+
+    test "sum of [1, 2, 3, 4, 5] is 15" do
+      list = [1, 2, 3, 4, 5]
+
+      response = SumList.call(list)
+
+      exepected_response = 15
+
+      assert response == exepected_response
+    end
   end
 end
